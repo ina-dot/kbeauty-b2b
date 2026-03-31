e6b56ced826cc3ab38edf21945933d69c798822c
ina-dot <ina@limesecos.com>
2026-03-31 13:42:45 +0900

    Initial commit: K-Beauty B2B wholesale platform

---
+++ .same/todos.md
@@ -0,0 +1,15 @@
+# K-Beauty B2B Project TODOs
+## Completed
+- [x] Created K-Beauty B2B homepage based on previous deployed version
+- [x] Added product images to brand cards (COSRX, Klairs, SOME BY MI, etc.)
+- [x] Implemented responsive brand grid layout with 14 brands
+- [x] Added hover effects on brand cards with image zoom
+- [x] Fixed image loading issues with reliable Unsplash URLs
+- [x] Added colored backgrounds for each brand card
+## In Progress
+- [ ] Could add more brand pages with detailed product listings
+## Future Enhancements
+- [ ] Add individual brand pages
+- [ ] Add product catalog functionality
+- [ ] Add authentication for B2B customers
+- [ ] Add shopping cart functionality
---
+++ next.config.js
@@ -0,0 +1,48 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ["*.preview.same-app.com"],
+  images: {
+    unoptimized: true,
+    domains: [
+      "source.unsplash.com",
+      "images.unsplash.com",
+      "ext.same-assets.com",
+      "ugc.same-assets.com",
+      "m.media-amazon.com",
+      "koreanskincare.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ext.same-assets.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ugc.same-assets.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "koreanskincare.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+module.exports = nextConfig;
